cb15eb88a05a09828e5550374d6a544231c75d6c
NGOGA Adolphe <adolphengoga@gmail.com>
2024-12-15 14:22:23 +0200

    registering a jwt strategy

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
+import { jwtStrategy } from './strategy';
 
 @Module({
-  providers: [AuthService],
+  providers: [AuthService,jwtStrategy],
   controllers: [AuthController],
   imports: [JwtModule.register({})],
 })
