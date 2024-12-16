1e67e7e2b5b63a79b07ccdde52dfcb75b2f106ab
NGOGA Adolphe <adolphengoga@gmail.com>
2024-12-16 15:19:55 +0200

    adding sending verification route

--- src/verification/verification.controller.ts
+++ src/verification/verification.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
+import { VerificationService } from './verification.service';
+import { Request } from 'express';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('verification')
-export class VerificationController {}
+export class VerificationController {
+  constructor(private verificationService: VerificationService) {}
+
+  @UseGuards(AuthGuard('jwt'))
+  @Get('')
+  verSender(@Req() req: Request) {
+    return this.verificationService.sendVer(req.user);
+  }
+}
--- src/verification/verification.service.ts
+++ src/verification/verification.service.ts
@@ -1,4 +1,41 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import * as nodemailer from 'nodemailer';
+import { verMessage } from './messages/verification.message';
+//import SMTPTransport from 'nodemailer/lib/smtp-transport';
 
 @Injectable()
-export class VerificationService {}
+export class VerificationService {
+  verificationCode = crypto.randomUUID().split('-')[0];
+
+  emailTransporter() {
+    const transporter = nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: parseInt(process.env.EMAIL_PORT),
+      secure: false,
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
+    return transporter;
+  }
+
+  async sendVer(user) {
+    const verificationLink = `${process.env.BASE_URL}/verify/${user.email}?OTP=${this.verificationCode}`;
+    const transporter = this.emailTransporter();
+    const options: nodemailer.SendMailOptions = {
+      from: process.env.EMAIL_USER,
+      to: user.email,
+      subject: 'Reminder: Confirm your email address',
+      html: verMessage(user,verificationLink,this.verificationCode),
+    };
+    try {
+      const isMessageSent = await transporter.sendMail(options);
+      return {
+        message: 'email sent'
+      }
+    } catch (error) {
+        throw new BadRequestException('incorrect email')
+    }  
+  }
+}
