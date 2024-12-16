348b30440c88c17438b783f398bfb1b2349490d5
NGOGA Adolphe <adolphengoga@gmail.com>
2024-12-16 17:09:26 +0200

    sending otp after registering

--- src/verification/verification.module.ts
+++ src/verification/verification.module.ts
@@ -1,9 +1,11 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { VerificationService } from './verification.service';
 import { VerificationController } from './verification.controller';
 
+@Global()
 @Module({
   providers: [VerificationService],
   controllers: [VerificationController],
+  exports: [VerificationService]
 })
 export class VerificationModule {}
