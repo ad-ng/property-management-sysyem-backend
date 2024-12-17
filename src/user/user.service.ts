e6885ccd0f976546d541fc2eabc7f45294de5990
NGOGA Adolphe <adolphengoga@gmail.com>
2024-12-17 09:59:21 +0200

    update user service

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,5 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
@@ -16,5 +17,37 @@ export class UserService {
       message: 'user found successfully',
       user: currentUser,
     };
+  }
+
+  async upddateUser(user, dto) {
+    try {
+      const currentUser = await this.prisma.user.update({
+        where: { email: user.email },
+        data: {
+          email: dto.email,
+          fullname: dto.fullname,
+          username: dto.username,
+          gender: dto.gender,
+          dob: dto.dob,
+          phoneNumber: dto.phoneNumber,
+          profileImg: dto.profileImg,
+        },
+      });
+      delete currentUser.password;
+      delete currentUser.verificationCode;
+      delete currentUser.createdAt;
+      delete currentUser.updatedAt;
+      return {
+        message: 'user updated successfully',
+        currentUser,
+      };
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code == 'P2002') {
+          throw new BadRequestException('email already taken');
+        }
+      }
+      return error;
+    }
   }
 }
