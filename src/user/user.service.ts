f1c65f91d9f0e368b6b0e24978682d0cbf44c6fd
NGOGA Adolphe <adolphengoga@gmail.com>
2024-12-17 09:43:33 +0200

    adding get current user route

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -3,14 +3,18 @@ import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class UserService {
-    constructor( private prisma: PrismaService){}
-    async current(user){
-        const currentUser = this.prisma.user.findUnique({
-            where: { email: user.email}
-        })
-        return {
-            message: "user found successfully",
-            user: currentUser
-        }
-    }
+  constructor(private prisma: PrismaService) {}
+  async current(user) {
+    const currentUser = await this.prisma.user.findUnique({
+      where: { email: user.email },
+    });
+    delete currentUser.password;
+    delete currentUser.verificationCode;
+    delete currentUser.createdAt;
+    delete currentUser.updatedAt;
+    return {
+      message: 'user found successfully',
+      user: currentUser,
+    };
+  }
 }
