9b56e9fd280478b8c51836fb760a021d715a3b91
NGOGA Adolphe <adolphengoga@gmail.com>
2024-12-17 09:27:17 +0200

    adding get currentuser service

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    constructor( private prisma: PrismaService){}
+    async current(user){
+        const currentUser = this.prisma.user.findUnique({
+            where: { email: user.email}
+        })
+        return {
+            message: "user found successfully",
+            user: currentUser
+        }
+    }
+}
