da2b7d211e8b474c91693e6a4c24dada7c24bde2
NGOGA Adolphe <adolphengoga@gmail.com>
2024-12-19 11:27:32 +0200

    commenting user controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -32,37 +32,60 @@ import { adminUserDTO } from './dto/user.admin.dto';
 export class UserController {
   constructor(private userService: UserService) {}
 
+    /*
+  _______________________________________________________________________________________________________________
+  :::::::::::::::::::::::::::::::::::::::::|                      |::::::::::::::::::::::::::::::::::::::::::::::
+  :::::::::::::::::::::::::::::::::::::::::| start of client role |::::::::::::::::::::::::::::::::::::::::::::::
+  :::::::::::::::::::::::::::::::::::::::::|                      |::::::::::::::::::::::::::::::::::::::::::::::
+  */
+
   /*
-  ___________________________________________________________________________________________________________________
-                                                      user.client
+  @IsVerifiedCheck(true) : this is for protecting routes to make sure that on verified user
+  .                      can access them
   */
 
   @IsVerifiedCheck(true)
-  @Get('me')
+  @Get('me') // route ----> GET  /user/me
   me(@Req() req: Request) {
     return this.userService.current(req.user);
   }
 
   @IsVerifiedCheck(true)
-  @Put('me')
+  @Put('me') // route ----> PUT  /user/me
   updateMe(@Req() req: Request, @Body() dto: UserClientDto) {
     return this.userService.upddateUser(req.user, dto);
   }
 
   @IsVerifiedCheck(true)
-  @Delete('me')
+  @Delete('me') // route ----> DELETE  /user/me
   deleteMe(@Req() req: Request) {
     return this.userService.deleteUser(req.user);
   }
 
   /*
-  ___________________________________________________________________________________________________________________
-                                                      user.admin
+  :::::::::::::::::::::::::::::::::::::::::|                    |::::::::::::::::::::::::::::::::::::::::::::::
+  :::::::::::::::::::::::::::::::::::::::::| end of client role |::::::::::::::::::::::::::::::::::::::::::::::
+  :::::::::::::::::::::::::::::::::::::::::|____________________|:::::::::::::::::::::::::::::::::::::::::::::::
   */
+  //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+  /*
+________________________________________________________________________________________________________________
+  .........................................| start of admin role |..............................................
+  _________________________________________|                     |______________________________________________
+  */
+
+    /*
+  @IsVerifiedCheck(true) : this is for protecting routes to make sure that on verified user
+  .                      can access them
+
+  @Roles(ROLE.admin)      // protecting routes with admin role
+  @UseGuards(RolesGuard)  // protecting routes with admin role
+  */
+
   @Roles(ROLE.admin)
-  @UseGuards(RolesGuard)
+  @UseGuards(RolesGuard) 
   @IsVerifiedCheck(true)
-  @Get('/admin/all')
+  @Get('/admin/all') //route ----> GET  /user/admin/all
   allUser(@Query() query: UserQueryDTO) {
     return this.userService.getAllUsers(query);
   }
@@ -70,7 +93,7 @@ export class UserController {
   @Roles(ROLE.admin)
   @UseGuards(RolesGuard)
   @IsVerifiedCheck(true)
-  @Get('/admin/:email')
+  @Get('/admin/:email') //route ----> GET  /user/admin/{email}
   userByEmail(@Param() param: DeleteUserDto) {
     return this.userService.getUserByEmail(param);
   }
@@ -78,7 +101,7 @@ export class UserController {
   @Roles(ROLE.admin)
   @UseGuards(RolesGuard)
   @IsVerifiedCheck(true)
-  @Post('/admin/add')
+  @Post('/admin/add') //route ----> POST  /user/admin/add
   addUser(@Body() dto: adminUserDTO) {
     return this.userService.createUser(dto);
   }
@@ -86,7 +109,7 @@ export class UserController {
   @Roles(ROLE.admin)
   @UseGuards(RolesGuard)
   @IsVerifiedCheck(true)
-  @Put('/admin/update')
+  @Put('/admin/update') //route ----> PUT  /user/admin/update
   updateUser(@Body() dto: UserAdminUpdateDTO) {
     return this.userService.adminUpdateUser(dto);
   }
@@ -94,7 +117,7 @@ export class UserController {
   @Roles(ROLE.admin)
   @UseGuards(RolesGuard)
   @IsVerifiedCheck(true)
-  @Delete('/admin/:email')
+  @Delete('/admin/:email') //route ----> DELETE  /user/admin/{email}
   adminDelete(@Param() param: DeleteUserDto) {
     return this.userService.adminDeleteUser(param);
   }
