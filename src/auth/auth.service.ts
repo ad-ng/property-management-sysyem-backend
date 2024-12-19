60c0b99777b09a415b1bf7d49e72d924e51fd4a6
NGOGA Adolphe <adolphengoga@gmail.com>
2024-12-19 13:47:31 +0200

    commenting auth service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -17,17 +17,28 @@ export class AuthService {
     private verification: VerificationService,
   ) {}
 
+  // log in  service
   async login(dto) {
+    //finding a user using entered email
     const currentUser = await this.prisma.user.findUnique({
       where: { email: dto.email },
     });
+
+    // in case no user found with given email
     if (!currentUser) throw new ForbiddenException('invalid credentials');
+
+    // comparing entered password with the hashed password from db
     const checkPassword: boolean = await argon.verify(
       currentUser.password,
       dto.password,
     );
+
+    // in case entered password does not match with hashed password in db
     if (!checkPassword) throw new ForbiddenException('invalid credentials');
+
+    // returning response to the user
     return {
+      // generating and returning a jwt using signToken() function we created at the bottom of this class
       access_token: await this.signToken(
         currentUser.id,
         currentUser.email,
@@ -38,18 +49,27 @@ export class AuthService {
     };
   }
 
+  // registering a user function
   async register(dto) {
+
+    //hashing the entered password for security purpose
     const hash: string = await argon.hash(dto.password);
+
     try {
+      // creating and saving a user in db
       const currentUser = await this.prisma.user.create({
         data: {
-          password: hash,
+          password: hash, // using hashed password rather than plain text password
           email: dto.email,
           username: dto.username,
           role: dto.role,
         },
       });
+
+      // sending verification code to the entered email address of a user
       this.verification.sendVer(currentUser);
+
+      // return response to the user
       return {
         access_token: await this.signToken(
           currentUser.id,
@@ -61,6 +81,7 @@ export class AuthService {
       };
     } catch (error) {
       if (error instanceof PrismaClientKnownRequestError) {
+        // in case auser is trying to use an already registered email
         if (error.code == 'P2002') {
           throw new BadRequestException('email already taken');
         }
@@ -68,6 +89,7 @@ export class AuthService {
     }
   }
 
+  // a function to generate a jwt  
   async signToken(
     id: number,
     email: string,
@@ -75,6 +97,7 @@ export class AuthService {
     username: string,
     isVerified: boolean,
   ) {
+    // data we want to include in a jwt
     const payload = {
       sub: id,
       email,
