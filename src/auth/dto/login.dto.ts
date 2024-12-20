d30418b4f2d23668bde1a7f043c3ed8703bb0526
NGOGA Adolphe <adolphengoga@gmail.com>
2024-12-20 09:06:24 +0200

    documenting auth DTO

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,10 +1,13 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class LogInDTO {
+  @ApiProperty({ example: 'johndoe@hotmail.com' })
   @IsNotEmpty()
   @IsEmail()
   email: string;
 
+  @ApiProperty({ example: 'test@123' })
   @IsNotEmpty()
   @IsString()
   password: string;
