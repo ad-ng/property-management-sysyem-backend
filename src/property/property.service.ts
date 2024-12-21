ddf0631bee0e1a9e2e1eccafa4d31f8dd68fd338
NGOGA Adolphe <adolphengoga@gmail.com>
2024-12-21 20:39:55 +0200

    creating new property

--- src/property/property.service.ts
+++ src/property/property.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class PropertyService {}
+export class PropertyService {
+    constructor( private prisma: PrismaService){}
+
+    async createProperty(dto,user){
+        const newProperty = await this.prisma.property.create({
+            data: {
+                title: dto.title,
+                description: dto.description,
+                ownerId: user.sub,
+                location: {
+                    create: {
+                        cell: {
+                            create: {
+                                name: dto.name
+                            }
+                        },
+
+                    }
+                }
+            }
+        })
+    }
+}
