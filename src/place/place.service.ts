f2dcaa4a0d4f94e0a62aa30777170fd6f48e797d
NGOGA Adolphe <adolphengoga@gmail.com>
2024-12-22 17:34:18 +0200

    referencing or creating a place

--- src/place/place.service.ts
+++ src/place/place.service.ts
@@ -3,13 +3,95 @@ import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class PlaceService {
-    constructor( private prisma: PrismaService){}
+  constructor(private prisma: PrismaService) {}
 
-    async addPlace(dto){
-        const { province, district, sector, cell } = dto
-        
-        const  placeProv = await this.prisma.province.findUnique({
-            where: { name: province}
-        })
+  async addPlace(dto) {
+    let myLocation = await this.prisma.place.findFirst({
+      where: {
+        country: {
+          name: dto.country,
+        },
+        province: {
+          name: dto.province,
+        },
+        district: {
+          name: dto.district,
+        },
+        sector: {
+          name: dto.sector,
+        },
+        cell: {
+          name: dto.cell,
+        },
+      },
+      include: {
+        country: true,
+        province: true,
+        district: true,
+        sector: true,
+        cell: true,
+      },
+    });
+
+    if (!myLocation) {
+      let myCountry = await this.prisma.country.findUnique({
+        where: { name: dto.country },
+      });
+      if (!myCountry) {
+        myCountry = await this.prisma.country.create({
+          data: { name: dto.country },
+        });
+      }
+
+      let myProv = await this.prisma.province.findUnique({
+        where: { name: dto.province },
+      });
+      if (!myProv) {
+        myProv = await this.prisma.province.create({
+          data: {
+            name: dto.province,
+          },
+        });
+      }
+
+      let myDistrict = await this.prisma.district.findUnique({
+        where: { name: dto.district },
+      });
+      if (!myDistrict) {
+        myDistrict = await this.prisma.district.create({
+          data: dto.district,
+        });
+      }
+
+      let mySector = await this.prisma.sector.findUnique({
+        where: { name: dto.sector },
+      });
+      if (!mySector) {
+        myDistrict = await this.prisma.sector.create({
+          data: { name: dto.sector },
+        });
+      }
+
+      let myCell = await this.prisma.cell.findUnique({
+        where: { name: dto.cell },
+      });
+      if (!myCell) {
+        myCell = await this.prisma.cell.create({
+          data: { name: dto.cell },
+        });
+      }
+
+      return await this.prisma.place.create({
+        data: {
+          cellId: myCell.id,
+          sectorId: mySector.id,
+          districtId: myDistrict.id,
+          provinceId: myProv.id,
+          countryId: myCountry.id,
+        },
+      });
     }
+
+    return myLocation;
+  }
 }
