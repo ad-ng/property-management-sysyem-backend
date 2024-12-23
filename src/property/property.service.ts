4739355a04e5c98d4b04b0c1bb546ca62345414e
NGOGA Adolphe <adolphengoga@gmail.com>
2024-12-23 07:44:11 +0200

    saving slug

--- src/property/property.service.ts
+++ src/property/property.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import slugify from 'slugify';
 import { PlaceService } from 'src/place/place.service';
 import { PrismaService } from 'src/prisma/prisma.service';
 
@@ -12,9 +13,18 @@ export class PropertyService {
   async createProperty(dto, user) {
     if (!dto.country) dto.country = 'rwanda'
     const myPlace = await this.placeService.addPlace(dto);
+    const mySlug = slugify(`${dto.title}`, {
+      replacement: '-', // replace spaces with replacement character, defaults to `-`
+      remove: undefined, // remove characters that match regex, defaults to `undefined`
+      lower: false, // convert to lower case, defaults to `false`
+      strict: false, // strip special characters except replacement, defaults to `false`
+      locale: 'vi', // language code of the locale to use
+      trim: true // trim leading and trailing replacement chars, defaults to `true`
+    })
     const newProperty = await this.prisma.property.create({
       data: {
         title: dto.title,
+        slug: mySlug,
         description: dto.description,
         ownerId: user.sub,
         locationId:  myPlace.id,
