edbf7c894b8d70201ac3e61ef6b425597e3b6482
NGOGA Adolphe <adolphengoga@gmail.com>
2024-12-24 17:13:13 +0200

    adding a seeder

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,5 +1,5 @@
 import { PrismaClient } from "@prisma/client"
-import { faker } from '@faker-js/faker';
+import { faker, tr } from '@faker-js/faker';
 import * as argon from 'argon2'
 
 const prisma =  new PrismaClient
@@ -26,6 +26,62 @@ async function main() {
         })
     }
     console.log('person seeds completed')
+
+    for(let i=1; i < 11; i++){
+      await prisma.place.create({
+        data: {
+          country: {
+            create: {
+              name: 'rwanda'
+            }
+          },
+          province: {
+            create: {
+              name: faker.location.city()
+            }
+          },
+          district: {
+            create: {
+              name: faker.location.state()
+            }
+          },
+          sector: {
+            create: {
+              name: faker.location.street()
+            }
+          },
+          cell: {
+            create: {
+              name: faker.location.secondaryAddress()
+            }
+          },
+        },
+        include: {
+          country: true,
+          province: true,
+          district: true,
+          sector: true,
+          cell: true
+        }
+      })
+    }
+    console.log('location seeds completed');
+    
+   
+    for(let i =1; i < 21; i++) {
+      await prisma.property.create({
+        data: {
+          title: faker.company.name(),
+          slug: faker.helpers.slugify(faker.book.title()),
+          description: faker.lorem.paragraph(),
+          locationId: faker.number.int({ min: 1, max: 10 }),
+          ownerId: faker.number.int({ min: 1, max: 10 }),
+          totalUnits: faker.number.int({ min: 1, max: 30 })
+        }
+      })
+    }
+    console.log('property seeds completed');
+    
 }
 
 main()
