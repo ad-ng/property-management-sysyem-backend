cf8b230658117966dfcc8114269561432c6817bf
NGOGA Adolphe <adolphengoga@gmail.com>
2024-12-27 10:02:19 +0200

    adding an apartment

--- src/apartment/apartment.controller.ts
+++ src/apartment/apartment.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { ApartmentService } from './apartment.service';
+import { AuthGuard } from '@nestjs/passport';
+import { Roles } from 'src/auth/decorators/role.decorator';
+import { ROLE } from '@prisma/client';
+import { IsVerifiedCheck } from 'src/auth/decorators/isverified.decorator';
+import { RolesGuard } from 'src/auth/gaurds/roles.guard.ts/roles.guard.ts.guard';
 
+@UseGuards(AuthGuard('jwt'))
 @Controller('apartment')
-export class ApartmentController {}
+export class ApartmentController {
+    constructor( private apartmentService: ApartmentService){}
+
+  @Roles(ROLE.admin,ROLE.manager,ROLE.owner)
+  @UseGuards(RolesGuard)
+  @IsVerifiedCheck(true)
+  @Post('')
+  addApartment(@Body() dto: any){
+    return this.apartmentService.saveApartment(dto)
+  }
+}
+
+
--- src/apartment/apartment.service.ts
+++ src/apartment/apartment.service.ts
@@ -1,4 +1,43 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import slugify from 'slugify';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class ApartmentService {}
+export class ApartmentService {
+    constructor(private prisma: PrismaService){}
+
+    async saveApartment(dto){
+    const mySlug = slugify(`${dto.apartmentName}`, {
+      replacement: '-', // replace spaces with replacement character, defaults to `-`
+      remove: undefined, // remove characters that match regex, defaults to `undefined`
+      lower: false, // convert to lower case, defaults to `false`
+      strict: false, // strip special characters except replacement, defaults to `false`
+      locale: 'vi', // language code of the locale to use
+      trim: true, // trim leading and trailing replacement chars, defaults to `true`
+    });
+     
+    const checkProperty = await this.prisma.property.findUnique({
+        where: { id: dto.propertyId }
+    })
+
+    if (!checkProperty) throw new NotFoundException('property not found')
+
+        try {
+            const newApartment = await this.prisma.apartment.create({
+            data: {
+                propertyId: dto.propertyId,
+                apartmentName: dto.apartmentName,
+                floor_number: dto.floor_number,
+                slug: mySlug,
+                status: dto.status
+            }
+        })
+        return {
+            message: 'apartment has been saved',
+            date: newApartment
+        }
+        } catch (error) {
+            return error
+        }
+    }
+}
