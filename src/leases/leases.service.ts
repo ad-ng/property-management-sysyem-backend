698e7b5fc543b2a92d4e6feb7b45e0eb743817eb
NGOGA Adolphe <adolphengoga@gmail.com>
2025-01-02 20:25:27 +0200

    checking user role before proceeding

--- src/leases/leases.service.ts
+++ src/leases/leases.service.ts
@@ -1,4 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class LeasesService {}
+export class LeasesService {
+    constructor( private prisma: PrismaService){}
+
+    async createLease(apartmentId,user){
+        const checkApt = await this.prisma.apartment.findUnique({
+                where: { id: apartmentId }, 
+                include: { property: true}
+            })
+        if(user.role == 'manager'){
+            if(checkApt.property.managerEmail != user.email) throw ForbiddenException
+        }
+
+        if(user.role == 'owner'){
+            if(checkApt.property.ownerId != user.sub) throw ForbiddenException
+        }
+    }
+}
