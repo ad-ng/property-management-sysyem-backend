ffc5d59ab9574d01caf69a3b9406d57c0c3a96fd
NGOGA Adolphe <adolphengoga@gmail.com>
2025-01-02 20:34:30 +0200

    finishing up create lease service

--- src/leases/leases.service.ts
+++ src/leases/leases.service.ts
@@ -1,3 +1,4 @@
+import { ne } from '@faker-js/faker/.';
 import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 
@@ -5,7 +6,7 @@ import { PrismaService } from 'src/prisma/prisma.service';
 export class LeasesService {
   constructor(private prisma: PrismaService) {}
 
-  async createLease(apartmentId, user) {
+  async createLease(apartmentId, dto, user) {
     const checkApt = await this.prisma.apartment.findUnique({
       where: { id: apartmentId },
       include: { property: true },
@@ -17,6 +18,31 @@ export class LeasesService {
 
     if (user.role == 'owner') {
       if (checkApt.property.ownerId != user.sub) throw ForbiddenException;
+    }
+
+    try {
+      const newLease = await this.prisma.leases.create({
+        data: {
+          lease_start_date: dto.lease_start_date,
+          lease_status: dto.lease_status,
+          apartmentId,
+          tenantEmail: dto.tenantEmail,
+          payment_due_day: dto.payment_due_day,
+          security_deposit: dto.security_deposit,
+          monthly_rent: dto.monthly_rent,
+          lease_end_date: dto.lease_end_date,
+        },
+      });
+      delete newLease.createdAt;
+      delete newLease.updatedAt;
+      delete newLease.id;
+
+      return {
+        message: 'lease created successfully',
+        data: newLease,
+      };
+    } catch (error) {
+      return error;
     }
   }
 }
